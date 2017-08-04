436042889db7000eef423ccff04bd6be13848731
Kien Huynh <KienHuynh@github.com>
2017-08-04 12:56:28 +0700

    added test function and update the net

--- count_with_dl.py
+++ count_with_dl.py
@@ -16,91 +16,81 @@ class RegressionCNN(nn.Module):
         super(RegressionCNN, self).__init__()
         # Load weight values from file
         self.vgg = {} 
-        self.vgg_conv1_1 = nn.Conv2d(3, 64, 3, bias=True, padding=1)
-        self.vgg_conv1_2 = nn.Conv2d(64, 64, 3, bias=True, padding=1)
+        self.vgg_conv1_1 = nn.Conv2d(3, 64, 3, bias=False, padding=1)
+        self.vgg_conv1_2 = nn.Conv2d(64, 64, 3, bias=False, padding=1)
         
-        self.vgg_conv2_1 = nn.Conv2d(64, 128, 3, bias=True, padding=1)
-        self.vgg_conv2_2 = nn.Conv2d(128, 128, 3, bias=True, padding=1)
+        self.vgg_conv2_1 = nn.Conv2d(64, 128, 3, bias=False, padding=1)
+        self.vgg_conv2_2 = nn.Conv2d(128, 128, 3, bias=False, padding=1)
         
-        self.vgg_conv3_1 = nn.Conv2d(128, 256, 3, bias=True, padding=1)
-        self.vgg_conv3_2 = nn.Conv2d(256, 256, 3, bias=True, padding=1)
-        self.vgg_conv3_3 = nn.Conv2d(256, 256, 3, bias=True, padding=1)
+        self.vgg_conv3_1 = nn.Conv2d(128, 256, 3, bias=False, padding=1)
+        self.vgg_conv3_2 = nn.Conv2d(256, 256, 3, bias=False, padding=1)
+        self.vgg_conv3_3 = nn.Conv2d(256, 256, 3, bias=False, padding=1)
         
-        self.vgg_conv4_1 = nn.Conv2d(256, 512, 3, bias=True, padding=1)
-        self.vgg_conv4_2 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
-        self.vgg_conv4_3 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
+        self.vgg_conv4_1 = nn.Conv2d(256, 512, 3, bias=False, padding=1)
+        self.vgg_conv4_2 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
+        self.vgg_conv4_3 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
 
-        self.vgg_conv5_1 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
-        self.vgg_conv5_2 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
-        self.vgg_conv5_3 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
+        self.vgg_conv5_1 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
+        self.vgg_conv5_2 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
+        self.vgg_conv5_3 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
         
-        self.vgg_conv6_1 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
-        self.vgg_conv6_2 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
-        self.vgg_conv6_3 = nn.Conv2d(512, 512, 3, bias=True, padding=1)
+        self.vgg_conv6_1 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
+        self.vgg_conv6_2 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
+        self.vgg_conv6_3 = nn.Conv2d(512, 512, 3, bias=False, padding=1)
 
-        self.vgg_fc6 = nn.Linear(8*8*512, 4096)
-        self.vgg_fc7 = nn.Linear(4096, 4096)
+        self.vgg_fc6 = nn.Linear(8*8*512, 4096, bias=False)
+        self.vgg_fc7 = nn.Linear(4096, 4096, bias=False)
         self.vgg_fc8 = nn.Linear(4096, 1, bias=False)
        
         self.vgg_pool = nn.MaxPool2d(2,2)
 	self.dropout01 = nn.Dropout(0.1)
-	self.dropout05 = nn.Dropout(0.5)
+	self.dropout02 = nn.Dropout(0.2)
 	self.dropout03 = nn.Dropout(0.3)
 	self.dropout04 = nn.Dropout(0.4)
+        self.dropout05 = nn.Dropout(0.5)
 
     def forward(self, x): 
-	x = self.dropout01(x)
-        x = F.relu(self.vgg_conv1_1(x))
-	x = self.dropout03(x)
+        #x = self.dropout01(x)
+        x = F.relu(self.vgg_conv1_1(x))	
         x = F.relu(self.vgg_conv1_2(x))
         x = self.vgg_pool(x)
         
-	x = self.dropout03(x)
+	#x = self.dropout03(x) 
         x = F.relu(self.vgg_conv2_1(x))
-        x = self.dropout03(x)
 	x = F.relu(self.vgg_conv2_2(x))
         x = self.vgg_pool(x)
 
-	x = self.dropout03(x)
+	#x = self.dropout03(x)
         x = F.relu(self.vgg_conv3_1(x))
-	x = self.dropout03(x)
         x = F.relu(self.vgg_conv3_2(x))
-	x = self.dropout03(x)
         x = F.relu(self.vgg_conv3_3(x))
         x = self.vgg_pool(x)
         
-	x = self.dropout04(x)
+	#x = self.dropout03(x)
         x = F.relu(self.vgg_conv4_1(x))
-	x = self.dropout04(x)
         x = F.relu(self.vgg_conv4_2(x))
-	x = self.dropout04(x)
         x = F.relu(self.vgg_conv4_3(x))
         x = self.vgg_pool(x)
 
-	x = self.dropout04(x)
+	#x = self.dropout03(x)
         x = F.relu(self.vgg_conv5_1(x))
-	x = self.dropout04(x)
         x = F.relu(self.vgg_conv5_2(x))
-	x = self.dropout04(x)
         x = F.relu(self.vgg_conv5_3(x))
         x = self.vgg_pool(x)
         
-	x = self.dropout05(x)
+	#x = self.dropout03(x)
         x = F.relu(self.vgg_conv6_1(x))
-	x = self.dropout05(x)
         x = F.relu(self.vgg_conv6_2(x))
-	x = self.dropout05(x)
         x = F.relu(self.vgg_conv6_3(x))
         x = self.vgg_pool(x)
 
-	x = self.dropout05(x)
+	#x = self.dropout02(x)
         x = x.view(-1, 8*8*512)
-        x = self.vgg_fc6(x)
-	x = self.dropout05(x)
-        x = self.vgg_fc7(x)
+        x = self.vgg_fc6(x)	
+        x = self.vgg_fc7(x) 
         x = self.vgg_fc8(x)
         
-        return x
+        return x 
 
 def grad_clip(net, max_grad = 0.1):
     params = [p for p in list(net.parameters()) if p.requires_grad==True]
@@ -123,7 +113,7 @@ def train():
     if (use_cuda):
         net.cuda()
     
-    init_lr = 0.0001
+    init_lr = 0.00005
     train_params = []
     for p in net.parameters(): 
         if (p.requires_grad):
@@ -140,8 +130,8 @@ def train():
     train_mean = h5_dict['mean'].astype(np.float32)
     
     # For debug purpose
-    #train_x = train_x[0:4,:,:,:]
-    #train_y = train_y[0:4,:]
+    #train_x = train_x[0:40,:,:,:]
+    #train_y = train_y[0:40,:]
 
     # Mean norm
     train_x = train_x - train_mean
@@ -150,33 +140,32 @@ def train():
     # Put these numpy arrays into torch tensor
     train_x = torch.from_numpy(train_x)
     train_y = torch.from_numpy(train_y)
-    test_x = torch.from_numpy(test_x)
-    test_y = torch.from_numpy(test_y)
+    #test_x = torch.from_numpy(test_x)
+    #test_y = torch.from_numpy(test_y)
 
     train_y = train_y[:,0]
     test_y = test_y[:,0]
  
     # Meta params
-    batch_size = 3
+    batch_size = 4
     num_train = train_x.size()[0]
-    num_test = test_x.size()[0]
     num_ite_per_e = int(np.ceil(float(num_train)/float(batch_size)))
     full_ind = np.arange(num_train)
     rng = np.random.RandomState(1311) 
     all_loss = []
-    num_e_to_reduce_lr = 50
+    num_e_to_reduce_lr = 25
     max_grad = 2 # For grad clip
 
     # Save params
-    save_freq = 15
+    save_freq = 40
     save_path = '../data/trained_model/traffic-regression-dl/dataset2/'
-    save_name = '24-07-17_net'
-    meta_name = '24-07-17_meta'
+    save_name = '03-08-17_net'
+    meta_name = '03-08-17_meta'
     save_list = glob.glob(save_path + save_name + '*.dat')
     last_e = 0
     
     # Printing and tracking params
-    num_ite_to_log = 10
+    num_ite_to_log = 1
   
     if (len(save_list) > 0): 
         save_list = sorted(save_list)[-1]
@@ -190,7 +179,7 @@ def train():
     
     print('Current learning rate: %f' % init_lr)
 
-    for e in range(last_e, 600): 
+    for e in range(last_e, 1000): 
         running_loss = 0.0
 
         # Divide lr by 1.5 after 200 epochv
@@ -214,10 +203,10 @@ def train():
             else: 
                 batch_x = Variable(train_x[torch.LongTensor(batch_range.tolist())])
                 batch_y = Variable(train_y[torch.LongTensor(batch_range.tolist())])
-
+ 
             outputs = net(batch_x)
-            #pdb.set_trace()
-	    loss = criterion(outputs, batch_y)
+	    #pdb.set_trace()
+            loss = criterion(outputs, batch_y)
             loss.backward()
             grad_clip(net, max_grad)
             optimizer.step()
@@ -230,9 +219,104 @@ def train():
                 running_loss = 0.0
 
         if (e % save_freq == (save_freq-1)):
+            abc = 1
             print('Saving at epoch %d' % e)
 	    torch.save({'state_dict': net.state_dict(), 'opt': optimizer.state_dict()}, save_path + save_name + ('_%03d' % e) + '.dat')
             SaveList([e, running_loss, all_loss], save_path + meta_name + ('_%03d' % e) + '.dat') 
 
+def test():
+    use_cuda = True
+
+    # Create network & related training objects
+    net = RegressionCNN()
+    if (use_cuda):
+        net.cuda() 
+
+    # Load data
+    h5_dict = LoadH5('../data/traffic-data/dataset2_count_aux.h5')
+    train_x = h5_dict['train_x'].astype(np.float32)
+    test_x = h5_dict['test_x'].astype(np.float32)
+    train_y = h5_dict['train_y'].astype(np.float32)
+    test_y = h5_dict['test_y'].astype(np.float32)
+    train_mean = h5_dict['mean'].astype(np.float32)
+    
+    # For debug purpose
+    #train_x = train_x[0:4,:,:,:]
+    #train_y = train_y[0:4,:]
+
+    num_test = test_y.shape[0]
+
+    # Mean norm 
+    test_x = test_x - train_mean
+    train_x = train_x - train_mean
+    
+    # Put these numpy arrays into torch tensor
+    train_x = torch.from_numpy(train_x)
+    test_x = torch.from_numpy(test_x)
+    test_y = torch.from_numpy(test_y)
+
+    test_y = test_y[:,0] 
+    test_pred = np.zeros((num_test, 1), dtype=np.float32)
+    # Meta params
+    batch_size = 2 
+    num_test = test_x.size()[0]
+    num_ite_per_e = int(np.ceil(float(num_test)/float(batch_size)))
+    full_ind = np.arange(num_test)
+    rng = np.random.RandomState(1311) 
+    all_loss = []
+
+    # Save params
+    save_freq = 40
+    save_path = '../data/trained_model/traffic-regression-dl/dataset2/'
+    save_name = '03-08-17_net' 
+    test_name = '03-08-17_test'
+    save_list = glob.glob(save_path + save_name + '*.dat')
+    last_e = 0
+    
+    # Printing and tracking params
+    num_ite_to_log = 10
+    
+    if (len(save_list) > 0): 
+        save_list = sorted(save_list)[-10]
+        print('Loading network save at %s' % save_list) 
+        loadobj = torch.load(save_list)
+        net.load_state_dict(loadobj['state_dict']) 
+    
+    net.train(False) 
+    #net.eval()
+    for i in range(num_ite_per_e):   
+        if (i+1)*batch_size <= num_test:
+            batch_range = range(i*batch_size, (i+1)*batch_size)
+        else:
+            batch_range = range(i*batch_size, num_test)
+        batch_range = full_ind[batch_range]
+        if (use_cuda):
+            batch_x = Variable(train_x[torch.LongTensor(batch_range.tolist())].cuda())
+            #batch_y = Variable(train_y[torch.LongTensor(batch_range.tolist())].cuda())
+        else: 
+            batch_x = Variable(test_x[torch.LongTensor(batch_range.tolist())])
+            #batch_y = Variable(test_y[torch.LongTensor(batch_range.tolist())])
+
+        outputs = net.forward(batch_x)
+        #pdb.set_trace()
+        test_pred[batch_range] = outputs.data.cpu().numpy()
+        
+    pdb.set_trace()
+    test_y = test_y.numpy().reshape((300,1))
+    ae = np.abs(test_pred - test_y)
+    mae = np.mean(ae)
+    mae_std = np.mean(np.sqrt(np.sum((ae - mae)**2)))
+    
+    re = np.abs(test_pred - test_y)/test_y
+    mre = np.mean(re)
+    mre_std = np.mean(np.sqrt(np.sum((re-mre)**2)))
+    print('MAE: %.3f' % mae)
+    print('MAE std: %.3f' % mae_std)
+    print('MRE: %.3f' % mre)
+    print('MRE std: %.3f' % mre_std)
+
+    SaveList([test_pred, test_y, mae, mae_std, mre, mre_std], save_path + 'test_result/' + test_name + '.dat')
+
 if __name__ == '__main__':
-    train() 
+    #train() 
+    test()
